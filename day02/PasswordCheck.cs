76514c616923b3c66f6ecd6ba8fa6d9a8d5cb68a
slemsvamp <silverljus@gmail.com>
2020-12-02 06:51:57 +0100

    Day02 solved. commit #2

---
+++ day02/PasswordCheck.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day02
+{
+    public struct PasswordCheck
+    {
+        public int First;
+        public int Second;
+        public string Letter;
+        public string Password;
+    }
+}
