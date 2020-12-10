6285c212854f0431561e5b1eb3b16cce65510d50
slemsvamp <silverljus@gmail.com>
2020-12-10 12:44:48 +0100

    Day10 solved.

--- day10/FirstStar.cs
+++ day10/FirstStar.cs
@@ -1,14 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace day10
 {
     public class FirstStar
     {
-        public static string Run(object parameter)
+        public static string Run(List<int> jolts)
         {
-            return string.Empty;
+            int joltageRating = 0;
+            var differences = new Dictionary<int, int>();
+
+            foreach (var jolt in jolts.OrderBy(j => j))
+            {
+                if (jolt <= joltageRating + 3)
+                {
+                    var diff = jolt - joltageRating;
+
+                    if (!differences.ContainsKey(diff))
+                        differences.Add(diff, 0);
+                    differences[diff] += 1;
+
+                    joltageRating = jolt;
+                }
+            }
+
+            if (!differences.ContainsKey(3))
+                differences.Add(3, 0);
+            differences[3] += 1;
+
+            return (differences[1] * differences[3]).ToString();
         }
     }
 }
--- day10/SecondStar.cs
+++ day10/SecondStar.cs
@@ -1,14 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace day10
 {
     public class SecondStar
     {
-        public static string Run(object parameter)
+        public static string Run(List<int> jolts)
         {
-            return string.Empty;
+            int index = 1;
+            var adapters = new int[jolts.Count + 2];
+            var memo = new long[adapters.Length];
+
+            adapters[0] = 0;
+            foreach (var jolt in jolts.OrderBy(j => j))
+                adapters[index++] = jolt;
+
+            adapters[jolts.Count + 1] = jolts.Max() + 3;
+            memo[adapters.Length - 1] = 1;
+
+            for (int playhead = adapters.Length - 2; playhead >= 0; playhead--)
+            {
+                long paths = 0;
+
+                for (int width = playhead + 1; width < adapters.Length; width++)
+                    if (adapters[width] - adapters[playhead] <= 3)
+                        paths += memo[width];
+                    else break;
+
+                memo[playhead] = paths;
+            }
+
+            return memo[0].ToString();
         }
     }
 }
