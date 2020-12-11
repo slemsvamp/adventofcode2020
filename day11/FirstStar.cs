0b0b4d8826b89d513424b226f5a1fc0835bc4d08
slemsvamp <silverljus@gmail.com>
2020-12-11 07:32:19 +0100

    Day11 solved.

--- day11/FirstStar.cs
+++ day11/FirstStar.cs
@@ -6,9 +6,76 @@ namespace day11
 {
     public class FirstStar
     {
-        public static string Run(object parameter)
+        public static int[] _dirX = new[] { -1, 0, 1, -1, 0, 1, -1, 0, 1 };
+        public static int[] _dirY = new[] { -1, -1, -1, 0, 0, 0, 1, 1, 1 };
+
+        public static string Run(string[] input)
         {
-            return string.Empty;
+            var height = input.Length;
+            var width = input[0].Length;
+
+            char[,] seatings = new char[width, height];
+            var newSeatings = new char[width, height];
+
+            for (var y = 0; y < height; y++)
+                for (var x = 0; x < width; x++)
+                    seatings[x, y] = input[y][x];
+
+            while (true)
+            {
+                for (int y = 0; y < height; y++)
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (x < 0 || x >= width || y < 0 || y >= height)
+                            continue;
+
+                        int emptySeatCount = 0;
+                        int occupiedSeatCount = 0;
+
+                        for (int direction = 0; direction < 9; direction++)
+                        {
+                            var checkX = x + _dirX[direction];
+                            var checkY = y + _dirY[direction];
+                            if (checkX < 0 || checkX >= width || checkY < 0 || checkY >= height)
+                                continue;
+
+                            if (seatings[checkX, checkY] == 'L')
+                                emptySeatCount++;
+
+                            if (seatings[checkX, checkY] == '#')
+                                occupiedSeatCount++;
+
+                            if (seatings[x, y] == 'L' && occupiedSeatCount == 0)
+                                newSeatings[x, y] = '#';
+                            else if (seatings[x, y] == '#' && occupiedSeatCount > 4)
+                                newSeatings[x, y] = 'L';
+                            else
+                                newSeatings[x, y] = seatings[x, y];
+                        }
+                    }
+
+                int changes = 0;
+
+                for (int y = 0; y < height; y++)
+                    for (int x = 0; x < width; x++)
+                    {
+                        if (seatings[x, y] != newSeatings[x, y])
+                            changes++;
+                        seatings[x, y] = newSeatings[x, y];
+                    }
+
+                if (changes == 0)
+                    break;
+            }
+
+            int result = 0;
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    if (seatings[x, y] == '#')
+                        result++;
+
+            return result.ToString();
         }
     }
 }
