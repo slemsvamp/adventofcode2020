fad0959f97938329973e7f47caff7185d071760e
slemsvamp <silverljus@gmail.com>
2020-12-25 06:51:20 +0100

    Day25 solved.

--- day25/FirstStar.cs
+++ day25/FirstStar.cs
@@ -6,9 +6,45 @@ namespace day25
 {
     public class FirstStar
     {
-        public static string Run(object parameter)
+        public static string Run()
         {
-            return string.Empty;
+            long publicKey1 = 14788856;
+            long publicKey2 = 19316454;
+
+            long loopSize = GetLoopSize(7, publicKey1);
+            long encKey = CalculateEncryptionKey(publicKey2, loopSize);
+
+            return encKey.ToString();
+        }
+
+        private static long GetLoopSize(long subjectNumber, long publicKey)
+        {
+            int loop = 0;
+            long value = 1;
+
+            do
+            {
+                value *= subjectNumber;
+                value %= 20201227;
+                loop++;
+            } while (value != publicKey);
+
+            return loop;
+        }
+
+        private static long CalculateEncryptionKey(long subjectNumber, long loopSize)
+        {
+            long value = 1;
+            int loop = 0;
+
+            while (true)
+            {
+                value *= subjectNumber;
+                value %= 20201227;
+                loop++;
+                if (loop == loopSize)
+                    return value;
+            }
         }
     }
 }
--- day25/Program.cs
+++ day25/Program.cs
@@ -6,32 +6,19 @@ namespace day25
     {
         static void Main(string[] args)
         {
-            var input = InputParser.Parse("input.txt");
-
             Console.WriteLine("---- Part 01 ----");
-            var part1 = FirstStar.Run(input);
+            var part1 = FirstStar.Run();
             Console.WriteLine($"Result: {part1}");
-
-            Console.WriteLine("---- Part 02 ----");
-            var part2 = SecondStar.Run(input);
-            Console.WriteLine($"Result: {part2}");
 
             Console.WriteLine("-----------------");
             Console.WriteLine($"1) Copy {part1} to Clipboard");
-            Console.WriteLine($"2) Copy {part2} to Clipboard");
 
             Console.WriteLine("Any) Quit");
 
             var key = Console.ReadKey();
 
             if (key.Key == ConsoleKey.D1)
-            {
                 WindowsClipboard.SetText(part1);
-            }
-            else if (key.Key == ConsoleKey.D2)
-            {
-                WindowsClipboard.SetText(part2);
-            }
         }
     }
 }
